db77d87d71ab41543980599fd846c0795c9a729c
lbeckman314 <lbeckman314@gmail.com>
2018-11-21 15:52:34 -0800

    updated centers materials search

--- search-handling.sql
+++ search-handling.sql
@@ -1,7 +1,15 @@
+SELECT C.name, C.street_number, C.street_direction, C.street_name, C.street_type, C.city, C.state, C.zip FROM centers_materials CM
+INNER JOIN centers C ON C.id = CM.CID
+INNER JOIN materials M ON M.id = CM.MID
+WHERE (M.name = 'tv');
+
+
+/*
 SELECT H.instructions FROM materials_handling MH 
 INNER JOIN materials M ON M.id = MH.mid
 INNER JOIN handlingInstructions H ON H.id = MH.HID
 WHERE (M.name = 'tv');
+*/
 
         -- sql = `SELECT * FROM materials WHERE (materials.name LIKE "%"?"%")`;
 
